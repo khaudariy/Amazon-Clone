0cf5e6f7514dce7bb9d0fbd2bb840904c3cd6d38
Hossam <hossamkhaudariy0@gmail.com>
2024-04-18 00:25:59 +0200

    Create class Brand , Image and Review

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from taggit.managers import TaggableManager
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 
@@ -15,7 +16,26 @@ class Product(models.Model):
     name = models.CharField(max_length=120)
     flag = models.CharField(max_length=10,choices=FLAGS_TYPES)
     price =models.FloatField()
+    image = models.ImageField(upload_to='product')
     sku = models.IntegerField()
     subtitle = models.TextField(max_length=500)
     description = models.TextField(max_length=50000)
+    brand = models.ForeignKey('Brand',related_name='product_brand',on_delete=models.SET_NULL,null=True)
     tags = TaggableManager()
+
+
+
+class ProductImage(models.Model):
+    product =models.ForeignKey(Product,related_name='product_image',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='productimages')
+
+class Brand(models.Model):
+    name =models.CharField(max_length=100)
+    image = models.ImageField(upload_to='brand')
+
+class Review(models.Model):
+    user = models.ForeignKey(User,related_name='review_name',on_delete=models.SET_NULL,null=True)
+    product = models.ForeignKey(Product,related_name='review_product',on_delete=models.CASCADE)
+    review = models.TextField(max_length=500)
+    rate=models.IntegerField(choices=[(i,i) for i in range(1,6)])
+    created_at = models.DateTimeField(default=timezone.now)
