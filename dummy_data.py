7124a6aa114217594f2f773978e9d8a34ed4d5e2
Hossam <hossamkhaudariy0@gmail.com>
2024-04-21 14:32:23 +0200

    add faker data to brands and products

---
+++ dummy_data.py
@@ -0,0 +1,46 @@
+import os ,django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+
+
+from faker import Faker
+import random
+from products.models import Product , Brand , Review
+
+def seed_brand(n):
+    fake = Faker()
+    images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']
+
+    for _ in range(n):
+        Brand.objects.create(
+            name = fake.name(),
+            image = f"brand/{images[random.randint(0,9)]}"
+        )
+
+
+
+
+
+def seed_products(n):
+    fake = Faker()
+    flag_types = ['New', 'Sale' , 'Feature']
+    brands = Brand.objects.all()
+    images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']
+    for _ in range(n):
+        Product.objects.create(
+            name = fake.name(),
+            flag = flag_types[random.randint(0,2)],
+            price = random.uniform(20.99,99.99),
+            image = f"product/{images[random.randint(0,9)]}",
+            sku = random.randint(100,1000000),
+            subtitle = fake.text(max_nb_chars=450),
+            description = fake.text(max_nb_chars=4000),
+            brand = brands[random.randint(0,len(brands)-1)],
+            
+        )
+
+seed_products(1000)
+
+
+def seef_reviews(n):
+    pass
