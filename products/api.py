4b6659fe6b3c5d12667a6e6ae056845ec19bdde7
Hossam Khaudariy <85705225+khaudariy@users.noreply.github.com>
2024-04-28 01:14:33 +0300

    add filter order - search

--- products/api.py
+++ products/api.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
+
+
 from .models import Product,Brand,Review,ProductImage
-
-
-
 from . import serializers
 from .pagination import Mypagination
 
@@ -12,8 +13,10 @@ from .pagination import Mypagination
 class ProductListAPI(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductListSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
     filterset_fields = ['flag', 'brand']
+    search_fields = ['name', 'subtitle','description']
+    ordering_fields = ['name', 'brand','price']
 
 
 class ProductDetailAPI(generics.RetrieveAPIView):
@@ -25,6 +28,8 @@ class BrandListAPI(generics.ListAPIView):
     queryset = Brand.objects.all()
     serializer_class = serializers.BrandListSerializer
     pagination_class = Mypagination
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter]
+    search_fields = ['name']
     
 
 class BrandDetailAPI(generics.RetrieveAPIView):
