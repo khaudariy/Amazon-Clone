66d2c47d8ad5b3e162b5b0f88ef7bb213015d049
Hossam Khaudariy <85705225+khaudariy@users.noreply.github.com>
2024-04-28 02:03:43 +0300

    add swagger

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
-from .models import Product,Brand
+from .models import Product,Brand,Review,ProductImage
 
 
 
 
+class ProductImageSerliazer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductImage
+        fields = ['image']
+
+class ProductReviewSerliazer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = ['user','rate','review','created_at']
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
@@ -29,10 +40,27 @@ class ProductListSerializer(serializers.ModelSerializer):
 
 class ProductDetailSerializer(serializers.ModelSerializer):
      brand = serializers.StringRelatedField()
+     review_count = serializers.SerializerMethodField(method_name='get_review_count')
+     avg_rate = serializers.SerializerMethodField(method_name='get_avg_rate')
+     image = ProductImageSerliazer(source='product_image',many=True)
+     reviews = ProductReviewSerliazer(source='review_product',many=True)
      class Meta:
        model = Product
        fields = '__all__'
+     def get_avg_rate(self,object):
+            total = 0 
+            reviews = object.review_product.all()
+            if len(reviews)>0:
+                for item in reviews:
+                    total+=item.rate
+                avg = total / len(reviews) 
+            else:
+                avg = 0      
+            return avg 
 
+     def get_review_count(self,object):
+            reviews = object.review_product.all().count()
+            return reviews 
 class BrandListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
