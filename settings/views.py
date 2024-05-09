86c6062d99ce2b8b9d51b21e68edccefc1209484
Hossam Khaudariy <85705225+khaudariy@users.noreply.github.com>
2024-05-09 18:08:35 +0300

    home Page feature and saled items

--- settings/views.py
+++ settings/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from products.models import Product , Brand , Review
+from django.db.models.aggregates import Count,Sum,Avg,Max,Min
 
 def home(request):
-    return render (request,'settings/home.html',{})
-# Create your views here.
+    new_products = Product.objects.filter(flag='New')[:10]
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+    feature_products = Product.objects.filter(flag='Feature')[:6]
+    brands = Brand.objects.annotate(product_count=Count('product_brand'))[:10]
+    reviews = Review.objects.all()
+    
+    
+    return render (request,'settings/home.html',{
+        'new_products' : new_products ,
+        'sale_products' : sale_products ,
+        'feature_products' : feature_products ,
+        'brands' : brands ,
+        'reviews' : reviews
+
+    })
+
