a83b2ced35f8f937f634d2215598112a34338580
Hossam Khaudariy <85705225+khaudariy@users.noreply.github.com>
2024-05-22 01:50:43 +0300

    show cart in base

--- project/settings.py
+++ project/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'settings.settings_context_pocessor.get_settings',
+                'orders.cart_context_processor.get_cart_data',
             ],
         },
     },
