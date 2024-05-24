51457ff06ff85579ef91408cd65aa7d8e999286d
Hossam Khaudariy <85705225+khaudariy@users.noreply.github.com>
2024-05-24 23:18:38 +0300

    apply coupon API

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import order_list , checkout , add_to_cart
-from .api import OrderListAPI,OrderDeetailAPI
+from .api import OrderListAPI,OrderDeetailAPI,ApplyCouponAPI
 
 urlpatterns = [
     path ('',order_list),
@@ -10,4 +10,5 @@ urlpatterns = [
     #api
     path('api/<str:username>/orders',OrderListAPI.as_view()),
     path('api/<str:username>/orders/<int:pk>',OrderDeetailAPI.as_view()),
+    path('api/<str:username>/apply-coupon',ApplyCouponAPI.as_view()),
 ]
